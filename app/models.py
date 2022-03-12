13c558c9bf9290b150f2419ebecb86d82390a076
danknjoroge <dank8505@gmail.com>
2022-03-12 10:58:15 +0300

    Added blog model

--- app/models.py
+++ app/models.py
@@ -9,3 +9,13 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User{self.username}'
+
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    date = db.Column(db.Date)
+
+    def __repr__(self):
+        return f'Blog{self.title}'
