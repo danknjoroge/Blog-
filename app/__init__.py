4857d2d22a8940d2ce75cf57356f2ec58dc03c51
danknjoroge <dank8505@gmail.com>
2022-03-12 14:27:28 +0300

    initialized login managr instsnce

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection='strong'
+login_manager.login_view = 'auth.login'
 
 '''create app function'''
 def create_app(config_name):
@@ -23,4 +27,7 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
     return app
