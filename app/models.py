74236ea84c2dee95d2bbabb3412dc97bf9e1d6bf
danknjoroge <dank8505@gmail.com>
2022-03-12 13:14:32 +0300

    added verify password function

--- app/models.py
+++ app/models.py
@@ -1,10 +1,26 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    blogs= db.relationship('Blog', backref='user', lazy='dynamic')
+
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
 
     def __repr__(self):
@@ -12,16 +28,23 @@ class User(db.Model):
 
 
 class Blog(db.Model):
+    __tablename__ = 'blogs'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
     description = db.Column(db.String(255))
     date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments= db.relationship('Comments', backref='blog', lazy='dynamic')
+
+
 
     def __repr__(self):
         return f'Blog{self.title}'
 
 class Comments(db.Model):
+    __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.String(255))
     madeby = db.Column(db.String(255))
     dateposted = db.Column(db.Date)
+    blog_id = db.Column(db.Integer, db.ForeignKey("blogs.id"))
--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User
+from app.models import User, Blog, Comments
+from flask_migrate import Migrate, MigrateCommand
 
 # create app instance
 app = create_app('development')
@@ -9,9 +10,13 @@ manager = Manager(app)
 manager.add_command('server', Server)
 
 
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User,Blog = Blog ,Comments = Comments)
 
 
 if __name__ == '__main__':
