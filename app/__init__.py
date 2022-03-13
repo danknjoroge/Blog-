09cfa188e9ffaf34668b8127c53bc61fc99bb9f2
danknjoroge <dank8505@gmail.com>
2022-03-13 23:25:06 +0300

    added custom styles to index.html

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_login import LoginManager
 from flask_mail import Mail
+# from flask_uploads import UploadSet, configure_uploads,IMAGES
+
+
 
 bootstrap = Bootstrap()
 
@@ -12,6 +15,7 @@ mail = Mail()
 login_manager = LoginManager()
 login_manager.session_protection='strong'
 login_manager.login_view = 'auth.login'
+
 
 '''create app function'''
 def create_app(config_name):
