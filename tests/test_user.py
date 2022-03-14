20ec8112c0eb644ce98d0b91a90b53435880809f
danknjoroge <dank8505@gmail.com>
2022-03-14 09:26:16 +0300

    added tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,5 +1,5 @@
 import unittest
-from app.models import User
+from app.models import Blog, Comments, User
 
 class UserTest(unittest.TestCase):
 
@@ -15,3 +15,34 @@ class UserTest(unittest.TestCase):
 
     def test_password_verification(self):
         self.assertTrue(self.new_user.verify_password('daniel'))
+
+
+
+class TestComments(unittest.TestCase):
+    def setUp(self):
+        self.comments = Comments(comment='We are safe')
+    
+    def tearDown(self):
+        Comments.query.delete()
+
+    def test_save_comment(self):
+        self.new_comment.save_comments()
+        self.assertTrue(len(Comments.query.all())>0)
+
+
+class TestBlog(unittest.TestCase):
+    def setUp(self):
+        self.blog = Blog(comment='We are safe')
+    
+    def tearDown(self):
+        Blog.query.delete()
+
+    def test_save_blog(self):
+        self.new_comment.save_blogs()
+        self.assertTrue(len(Blog.query.all())>0)
+
+
+
+
+
+
